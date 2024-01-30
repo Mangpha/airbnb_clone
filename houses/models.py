2ff0a7b5cfeda300ff10e59f29e752b5aafeb722
Mangpha <8350130@gmail.com>
2024-01-30 13:45:07 +0900

    House Model Migrations

--- houses/models.py
+++ houses/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class House(models.Model):
+    """
+    House Model Definition
+    """
+
+    name = models.CharField(max_length=140)
+    price = models.PositiveIntegerField()
+    description = models.TextField()
+    address = models.CharField(max_length=140)
