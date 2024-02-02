332a50e989c464ab731d6e0198b3d46688b6c643
Mangpha <8350130@gmail.com>
2024-02-02 15:39:07 +0900

    Django templates

--- rooms/views.py
+++ rooms/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Room
 
 # Create your views here.
 
 
 def see_all_rooms(request):
-    return HttpResponse("See All rooms")
+    rooms = Room.objects.all()
+    return render(
+        request,
+        "all_rooms.html",
+        {
+            "rooms": rooms,
+            "title": "this title comes from django.",
+        },
+    )
 
 
 def see_one_room(request, room_id):
