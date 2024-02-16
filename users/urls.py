b2c445a55d4880fb2898a7af495022685397e7ec
Mangpha <8350130@gmail.com>
2024-02-16 16:15:19 +0900

    Create User

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.Users.as_view()),
     path("me/", views.Me.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 # DRF Imports
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, exceptions
 from rest_framework.permissions import IsAuthenticated
 
 # Serializer Imports
@@ -25,6 +25,23 @@ class Me(APIView):
         )
         if serializer.is_valid():
             user = serializer.save()
+            serializer = user_serial.PrivateUserSerializer(user)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+
+class Users(APIView):
+
+    def post(self, request):
+        password = request.data.get("password")
+        if not password:
+            raise exceptions.ParseError
+        serializer = user_serial.PrivateUserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            user.set_password(password)
+            user.save()
             serializer = user_serial.PrivateUserSerializer(user)
             return Response(serializer.data)
         else:
