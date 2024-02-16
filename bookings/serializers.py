c65fca18895cc687f1854330bc64fb16dd39c00e
Mangpha <8350130@gmail.com>
2024-02-16 14:51:44 +0900

    Create booking

--- bookings/serializers.py
+++ bookings/serializers.py
@@ -1,8 +1,37 @@
+# Django Imports
+from django.utils import timezone
+
 # DRF Imports
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, DateField, ValidationError
 
 # Model Imports
 from .models import Booking
+
+
+class CreateRoomBookingSerializer(ModelSerializer):
+
+    check_in = DateField()
+    check_out = DateField()
+
+    class Meta:
+        model = Booking
+        fields = (
+            "check_in",
+            "check_out",
+            "guests",
+        )
+
+    def validate_check_in(self, value):
+        now = timezone.localtime(timezone.now()).date()
+        if now > value:
+            raise ValidationError("Can't book in the past.")
+        return value
+
+    def validate_check_out(self, value):
+        now = timezone.localtime(timezone.now()).date()
+        if now > value:
+            raise ValidationError("Can't book in the past.")
+        return value
 
 
 class PublicBookingSerializer(ModelSerializer):
