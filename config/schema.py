1855217d411fca9682ac35411c9b215d56bd518c
Mangpha <8350130@gmail.com>
2024-02-20 16:24:34 +0900

    Query Arguments

--- config/schema.py
+++ config/schema.py
@@ -1,12 +1,31 @@
+from typing import List
+
 import strawberry
+
+
+@strawberry.type
+class Movie:
+    id: int
+    title: str
+    year: int
+    rating: float
+
+
+movies_db = [
+    Movie(id=1, title="test", year=2020, rating=4.5),
+]
 
 
 @strawberry.type
 class Query:
 
     @strawberry.field
-    def ping(self) -> str:
-        return "pong"
+    def movies(self) -> List[Movie]:
+        return movies_db
+
+    @strawberry.field
+    def movie(self, movie_id: int) -> Movie:
+        return movies_db[movie_id - 1]
 
 
 schema = strawberry.Schema(query=Query)
