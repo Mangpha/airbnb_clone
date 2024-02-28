65630fa3dcf4fb5f2a7aec2b1d3ef19b497c2dcb
Mangpha <8350130@gmail.com>
2024-02-28 16:00:13 +0900

    Kakao Login

--- users/urls.py
+++ users/urls.py
@@ -16,5 +16,6 @@ urlpatterns = [
     path("token-login", obtain_auth_token),
     path("jwt-login", views.JwtLogin.as_view()),
     path("github", views.GithubLogin.as_view()),
+    path("kakao", views.KakaoLogin.as_view()),
     path("@<str:username>/", views.PublicUser.as_view()),
 ]
